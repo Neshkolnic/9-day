9d24539de4f01c6cbd73622f44f42badf233a1e0
Admin <Admin@DESKTOP-TS0M18F>
2024-04-17 09:22:10 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace EquationSolver
+{
+    public abstract class Solution
+    {
+        public abstract string CalculateRoots();
+        public abstract string PrintRoots();
+    }
+
+    public class Linear : Solution
+    {
+        private double a, b;
+
+        public Linear(double a_val, double b_val)
+        {
+            a = a_val;
+            b = b_val;
+        }
+
+        public override string CalculateRoots()
+        {
+            if (a == 0)
+            {
+                
+                return "Not a linear equation";
+            }
+            double root = -b / a;
+            return root.ToString();
+        }
+
+        public override string PrintRoots()
+        {
+            return "Linear Equation: " + a + "x + " + b + " = 0";
+        }
+    }
+
+    public class Square : Solution
+    {
+        private double a, b, c;
+
+        public Square(double a_val, double b_val, double c_val)
+        {
+            a = a_val;
+            b = b_val;
+            c = c_val;
+        }
+
+        public override string CalculateRoots()
+        {
+            double discriminant = b * b - 4 * a * c;
+            if (discriminant > 0)
+            {
+                double root1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double root2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+               
+                return "Roots: " + root1 + ", " + root2;
+            }
+            else if (discriminant == 0)
+            {
+                double root = -b / (2 * a);
+               
+                return "Root: " + root;
+            }
+            else
+            {
+               
+                return "No real roots";
+            }
+        }
+
+        public override string PrintRoots()
+        {
+            return "Quadratic Equation: " + a + "x^2 + " + b + "x + " + c + " = 0";
+        }
+    }
+
+    public class Series
+    {
+        private List<Solution> solutions;
+
+        public Series()
+        {
+            solutions = new List<Solution>();
+        }
+
+        public void AddSolution(Solution sol)
+        {
+            solutions.Add(sol);
+        }
+
+        public void PrintCharacteristics()
+        {
+            foreach (Solution sol in solutions)
+            {
+                sol.PrintRoots();
+                sol.CalculateRoots();
+            }
+        }
+
+        public void SortSolutions()
+        {
+            solutions.Sort((sol1, sol2) => sol1.ToString().CompareTo(sol2.ToString()));
+        }
+
+        public void SaveToFile(string filename)
+        {
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                foreach (Solution sol in solutions)
+                {
+                    writer.WriteLine(sol.PrintRoots());
+                    writer.WriteLine(sol.CalculateRoots());
+                    writer.WriteLine();
+                }
+            }
+            MessageBox.Show("Data saved to " + filename);
+        }
+
+        public void LoadFromFile(string filename)
+        {
+            // Implement loading from file
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using EquationSolver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace _9_day
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Series series = new Series();
+        
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                label4.Visible = false;
+                textBox3.Visible = false;
+                label5.Text = "ax+b=0";
+                label5.Visible = true;
+            }
+            else
+            {
+                label4.Visible = true;
+                textBox3.Visible = true;
+                label5.Text = "ax^2+bx+c=0";
+                label5.Visible = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            if (double.TryParse(textBox1.Text, out a) && double.TryParse(textBox2.Text, out b))
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    // Линейное уравнение
+                    Linear linearEquation = new Linear(a, b);
+                    series.AddSolution(linearEquation);
+                    MessageBox.Show("Linear equation has already been added");
+                    listBox1.Items.Add(linearEquation.PrintRoots());
+                    listBox1.Items.Add(linearEquation.CalculateRoots());
+                }
+                else
+                {
+                    // Проверяем, введено ли значение для c
+                    if (double.TryParse(textBox3.Text, out c))
+                    {
+                        // Квадратное уравнение
+                        Square squareEquation = new Square(a, b, c);
+                        series.AddSolution(squareEquation);
+                        MessageBox.Show("Square equation has already been added");
+                        listBox1.Items.Add(squareEquation.PrintRoots());
+                        listBox1.Items.Add(squareEquation.CalculateRoots());
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a valid value for c.");
+                    }
+                    // Отображение уравнений в ListBox
+                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid values for a and b.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                series.SaveToFile(saveFileDialog.FileName);
+                MessageBox.Show("Equations saved to file.");
+            }
+        }
+    }
+}
